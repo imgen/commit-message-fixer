db87e7f599f71666a5d7264afa0af5036d10b0b1
billshu <bill.shu@gravitysupplychain.com>
2018-12-19 15:16:21 +0800

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,9 +1,15 @@
 @echo off
-@echo Building windows executable
-@go build cmf.go
-echo Building OSX executable
 
+@echo Building windows executable
+@set GOOS=windows
+@set GOARCH=amd64
+@go build cmf.go
+
+echo Building OSX executable
 @set GOOS=darwin
 @go build cmf.go
+
+REM set back GOOS to be windows
 @set GOOS=windows
+
 @echo on
