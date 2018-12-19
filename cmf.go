de6065d8f66f66c5249a8e787b2d018ad4282299
billshu <bill.shu@gravitysupplychain.com>
2018-12-19 18:40:26 +0800

    Refactoring to make it testable

--- cmf.go
+++ cmf.go
@@ -9,9 +9,37 @@ import (
 
 func main() {
 	filePath := os.Args[1]
-	content, _ := ioutil.ReadFile(filePath)
-	commitMessage := string(content)
+	commitMessage := getFileAsString(filePath)
 
+	branchName := getBranchName()
+	jiraIssueID := getJiraIssueID(branchName)
+
+	if commitMessage = fixCommitMessage(commitMessage, jiraIssueID); commitMessage != "" {
+		println("Commit message doesn't start with JIRA issue ID, correcting...")
+		if err := ioutil.WriteFile(filePath, []byte(commitMessage), 0644); err != nil {
+			println("Failed to write file " + filePath + ", error: " + err.Error())
+		} else {
+			println("Commit message fixed")
+		}
+	} else {
+		println("Commit message is ok")
+	}
+}
+
+func fixCommitMessage(commitMessage string, jiraIssueID string) string {
+	if strings.HasPrefix(commitMessage, jiraIssueID) {
+		return ""
+	}
+	return jiraIssueID + " - " + commitMessage
+}
+
+func getFileAsString(commitFilePath string) string {
+	content, _ := ioutil.ReadFile(commitFilePath)
+	commitMessage := string(content)
+	return commitMessage
+}
+
+func getBranchName() string {
 	cmdName := "git"
 	cmdArgs := strings.Split("rev-parse --abbrev-ref HEAD", " ")
 	var (
@@ -27,19 +55,12 @@ func main() {
 	strOutput := string(cmdOut)
 	branchName := strings.Split(strOutput, "\n")[0]
 	println("Branch name is " + branchName)
+	return branchName
+}
+
+func getJiraIssueID(branchName string) string {
 	parts := strings.Split(branchName, "-")
 	jiraIssueID := parts[0] + "-" + parts[1]
 	println("Jira issue id is " + jiraIssueID)
-
-	if !strings.HasPrefix(commitMessage, jiraIssueID) {
-		println("Commit message doesn't start with JIRA issue ID, correcting...")
-		commitMessage = jiraIssueID + " - " + commitMessage
-		if err := ioutil.WriteFile(filePath, []byte(commitMessage), 0644); err != nil {
-			println("Failed to write file " + filePath + ", error: " + err.Error())
-		} else {
-			println("Commit message fixed")
-		}
-	} else {
-		println("Commit message is ok")
-	}
+	return jiraIssueID
 }
